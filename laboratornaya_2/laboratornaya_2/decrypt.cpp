4e0cb7c9ab21afce5c98c6ec65a73a359a2c9235
AdzhiniazovSaid <106696913+AdzhiniazovSaid@users.noreply.github.com>
2022-06-27 22:57:12 +0300

    Add files via upload

---
+++ laboratornaya_2/laboratornaya_2/decrypt.cpp
@@ -0,0 +1,113 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <conio.h>
+#include <clocale>
+
+using namespace std;
+
+enum Menu {
+    close,
+    decrypting
+};
+
+void decrypt()
+{
+    cout << "|------------РАСШИФРОВКА------------|" << endl;
+
+    string decrypt_text, key, D = "";
+    string A = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    setlocale(LC_ALL, "Rus");
+    cout << "\n[DEC] Введите слово: ";
+    cin >> decrypt_text;
+    cout << "\n[DEC] Введите ключ: ";
+    cin >> key;
+    int* F = new int[decrypt_text.size()];
+    int* G = new int[decrypt_text.size()];
+    int c = key.size();
+    int b = decrypt_text.size();
+
+    if (b >= c)
+    {
+        for (int i = 0; i < (b / c); i++)
+        {
+            D = D + key;
+        }
+        for (int j = 0; j < (b % c); j++)
+        {
+            D = D + key[j];
+        }
+    }
+    else
+    {
+        for (int s = 0; s < b; s++)
+        {
+            D = D + decrypt_text[s];
+        }
+    }
+
+    for (int k = 0; k < b; k++)
+    {
+        for (int n = 0; n < 33; n++)
+        {
+            if (decrypt_text[k] == A[n])
+            {
+                F[k] = n;
+            }
+
+            if (D[k] == A[n])
+            {
+                G[k] = n;
+            }
+        }
+    }
+
+    int e = 0;
+    for (int u = 0; u < b; u++)
+    {
+        e = ((F[u] - G[u]));
+        if (e < 0)
+            e += 33;
+        decrypt_text[u] = A[e];
+    }
+
+    cout << "[DEC] Зашифрованный текст: " << decrypt_text << endl;
+}
+
+int main()
+{
+    system("chcp 1251");
+    
+    int choose;
+    bool tryes;
+
+    cout << "\n|------------МЕНЮ------------|" << endl;
+    cout << "\n\t[DEC] 0. Выход " << "\n\t[DEC] 1. Расшифровка ";
+    cout << "\n\nВаш выбор: ";
+    cin >> choose;
+
+    do {
+        switch (choose)
+        {
+        case close:
+            system("cls");
+            cout << "[MESSAGE] Вы вышли !";
+            exit(0);
+
+        case decrypting:
+            system("cls");
+            decrypt();
+            break;
+
+        default:
+            system("cls");
+            cout << "[MESSAGE] Неккоректный выбор ";
+            break;
+        }
+
+        cout << "\nХотите вернуться в меню [1 - Да | 0 - Нет]" ;
+        cout << "\n\nВаш выбор: ";
+        cin >> tryes;
+
+    } while (tryes != 0);
+}
